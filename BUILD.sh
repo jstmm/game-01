9b8c120b2b3c4cad84b50398fc9314d0737bcd69
Julien Stamm <5460657+jstmm@users.noreply.github.com>
2025-01-17 22:53:08 +0000

    Codebase improvement

--- BUILD.sh
+++ BUILD.sh
@@ -5,7 +5,13 @@ run_command() {
     "$@"
 }
 
-run_command cmake -S . -G "Unix Makefiles" -B build
-# run_command cmake -S . -B build -DCMAKE_BUILD_TYPE=Release
+if [ "$1" == "RELEASE" ]; then
+    run_command cmake -S . -B build -G Ninja -DCMAKE_BUILD_TYPE=Release
+else
+    run_command cmake -S . -B build -G Ninja -DCMAKE_BUILD_TYPE=Debug
+fi
 
 run_command cmake --build build
+
+# run_command meson setup build
+# run_command meson compile -C build
